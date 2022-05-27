a30cd80be15fac55b13665c2ff6f54c5aa1d5311
ztashneem <z.tashneem12@gmail.com>
2022-05-27 18:12:53 +0600

    single tools products added and single product fatch

--- index.js
+++ index.js
@@ -8,10 +8,34 @@ app.use(cors());
 
 
 
-const uri = "mongodb+srv://sareen-archways:xtnv7mGj4tsr3EKT@cluster0.s4umi.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x10dc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+const run = async () =>{
+    try{
+        await client.connect();
+        console.log("Connected to MongoDB");
+        const db = client.db("toolsDB");
+        const toolsCollection = db.collection("toolsCollection");
+
+            //API to get all tools
+    app.get("/tools", async (req, res) => {
+        const tools = await toolsCollection.find({}).toArray();
+        res.send(tools);
+      });
+  
+      //API to get single tools
+      app.get("/tools/:id", async (req, res) => {
+        const id = req.params.id;
+        const tool = await toolsCollection.findOne({ _id: ObjectId(id) });
+        res.send(tool);
+      });
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
