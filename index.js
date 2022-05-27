da7a25357ad030bf75e1bd8616aa71e0fa79a03b
ztashneem <z.tashneem12@gmail.com>
2022-05-27 18:14:14 +0600

    order , user oder collect by email

--- index.js
+++ index.js
@@ -30,7 +30,64 @@ const run = async () =>{
         const tool = await toolsCollection.findOne({ _id: ObjectId(id) });
         res.send(tool);
       });
+  ////API to get all orders
+  app.get("/orders", async (req, res) => {
+    const orders = await ordersCollection.find({}).toArray();
+    res.send(orders);
+  });
+
+  //API to update a order
+  app.put("/orders/:id", async (req, res) => {
+    const orderId = req.params.id;
+    const order = req.body;
+    console.log("order", order);
+    const query = { _id: ObjectId(orderId) };
+    const options = { upsert: true };
+    const updatedOrder = await ordersCollection.updateOne(
+      query,
+      {
+        $set: order,
+      },
+      options
+    );
+    res.send(updatedOrder);
+  });
+
+  //API to get orders by user email
+  app.get("/orders/:email", async (req, res) => {
+    const email = req.params.email;
+    const orders = await ordersCollection
+      .find({ userEmail: email })
+      .toArray();
+    res.send(orders);
+  });
+  //API to get orders with multiple query parameters
+  app.get("/orders/:email/:isdelivered", async (req, res) => {
+    const email = req.params.email;
+    const isdelivered = req.params.isdelivered;
+    const orders = await ordersCollection
+      .find({ userEmail: email, isDelivered: true })
+      .toArray();
+    res.send(orders);
+  });
+
+  //API to add a order
+  app.post("/orders", async (req, res) => {
+    const order = req.body;
+    const result = await ordersCollection.insertOne(order);
+    res.send(result);
+  });
+
+  //API to delete a order
+  app.delete("/orders/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log("id", id);
+    const result = await ordersCollection.deleteOne({ _id: ObjectId(id) });
+    res.send(result);
+  });
+
     }
+    
     finally{
 
     }
